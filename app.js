374ef322326788f9c26efa44fa2b95020a3fa446
LenizeM <lenizemalheiros@gmail.com>
2025-02-19 16:47:30 -0300

    Ajustes e melhorias

--- app.js
+++ app.js
@@ -41,6 +41,6 @@ function reiniciar() {
     document.getElementById('quantidade').value = '';
     document.getElementById('de').value = '';
     document.getElementById('ate').value = '';
-    document.getElementById('resultado').innerHTML = '                    <label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
     alterarStatusBotao();
 }
